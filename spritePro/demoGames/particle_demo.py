55fbd146da3d6741554e85b6d2a6b64d7ccd1634
Neoxider <232359023+bochkarnikov-victor@users.noreply.github.com>
2025-10-04 22:31:43 +0500

    particles update

--- spritePro/demoGames/particle_demo.py
+++ spritePro/demoGames/particle_demo.py
@@ -18,19 +18,19 @@ def main():
 
     emitter = ParticleEmitter(
         ParticleConfig(
-            amount=40,
-            size_range=(4, 7),
-            speed_range=(120, 220),
-            lifetime_range=(500, 900),
+            amount=100,
+            size_range=(2, 8),
+            speed_range=(50, 300),
+            lifetime_range=(500, 2000),
             colors=[(255, 200, 40), (255, 120, 200)],
-            gravity=Vector2(0, 20),
+            gravity=Vector2(0, 500),
         )
     )
 
     running = True
     text = s.TextSprite("Click on screen", 56, pos = s.WH_C)
     while running:
-        s.update(fill_color=(20, 20, 40), update_display=False)
+        s.update(fill_color=(20, 20, 40))
 
         for event in s.events:
             if event.type == pygame.QUIT:
@@ -38,10 +38,6 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 emitter.emit(event.pos)
                 text.set_active(False)
-
-        pygame.display.flip()
-
-    pygame.quit()
 
 
 if __name__ == "__main__":
