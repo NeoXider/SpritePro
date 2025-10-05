b6926cd1062a566304b66b80db516868e4a21bde
Neoxider <232359023+bochkarnikov-victor@users.noreply.github.com>
2025-10-05 15:25:03 +0500

    сclicker add

---
+++ clicker.py
@@ -0,0 +1,53 @@
+import spritePro as s
+import pygame 
+
+def on_click():
+    global score
+    score +=1
+    text.set_text(f"score: {score}")
+    text.set_active(True)
+    emitter.emit(button.rect.center)
+
+
+# Initialize the library
+s.init()
+
+# Create a window
+s.get_screen((1280, 960), "My Game")
+
+score = 0
+
+bg = s.Sprite("", s.WH, s.WH_C)
+bg.set_color((200,0,180))
+bg.set_screen_space(True)
+# text
+text = s.TextSprite("", 74, (255, 255, 255), s.WH_C)
+text.set_text(f"score: {score}")
+text.rect.y = 50
+text.set_active(False)
+text.set_screen_space(True)
+button = s.Button(
+    "",
+    (300, 300),
+    s.WH_C,
+    "Click Me",
+    36,
+    on_click=on_click,
+)
+
+emitter = s.ParticleEmitter(
+    s.ParticleConfig(
+        amount=100,
+        size_range=(5, 15),
+        speed_range=(50, 300),
+        lifetime_range=(500, 2000),
+        colors=[(255, 200, 40), (255, 120, 200)],
+        gravity=pygame.math.Vector2(0, 0),
+    )
+)
+
+# Main game loop
+while True:
+    s.update(fill_color=(0, 0, 100))
+    s.process_camera_input()
+    button.limit_movement(bg.rect)
