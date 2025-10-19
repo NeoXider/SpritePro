c5373e480147b0fc6bea9dfe8732ca66bab8bbee
Neoxider <232359023+bochkarnikov-victor@users.noreply.github.com>
2025-10-19 17:36:01 +0500

    update demogames

--- spritePro/demoGames/amongus.py
+++ spritePro/demoGames/amongus.py
@@ -5,7 +5,7 @@ from pathlib import Path
 current_dir = Path(__file__).parent
 parent_dir = current_dir.parent.parent
 sys.path.append(str(parent_dir))
-#====================================================
+# ====================================================
 path = Path(__file__).parent
 
 import spritePro as s
@@ -16,14 +16,16 @@ def on_click():
     global k_space
     k_space = True
 
+
 def on_canistra():
     global cacanistra_count
-    cacanistra_count+=1
+    cacanistra_count += 1
     print(cacanistra_count)
-    canistra_bar.set_image("", (200, cacanistra_count/canistra_max*250))
-    if cacanistra_count >= canistra_max+1:
+    canistra_bar.set_image("", (200, cacanistra_count / canistra_max * 250))
+    if cacanistra_count >= canistra_max + 1:
         canistra_bg.set_active(False)
         canistra_bar.set_active(False)
+
 
 s.init()
 
@@ -31,35 +33,46 @@ s.get_screen((1280, 960))
 
 bg = s.Sprite("spritePro\demoGames\Sprites\map.jpg")
 bg.set_native_size()
+bg.set_sorting_order(-1000)
 player = s.Sprite("spritePro\demoGames\Sprites\\amogus.png")
 player.set_native_size()
-player.set_position((400,300))
+player.set_position((400, 300))
 player.set_scale(0.07)
 player.speed = 5
+player.collide = s.Sprite("", (30,50), player.rect.center)
+player.collide.set_parent(player)
+player.collide.set_alpha(50)
 
 text_cord = s.TextSprite("", 46)
 text_cord.set_position((s.WH_C.x, 10), s.Anchor.MID_TOP)
 text_cord.set_screen_space(True)
 
-t1 = s.Sprite("", (50,50), (810, 610))
+t1 = s.Sprite("", (50, 50), (810, 610))
 t1.set_alpha(50)
-t1.set_color((0,0,255))
+t1.set_color((0, 0, 255))
 
-t2 = s.Sprite("", (50,50), (332, 295))
+t2 = s.Sprite("", (50, 50), (332, 295))
 t2.set_alpha(50)
-t2.set_color((255,0,0))
+t2.set_color((255, 0, 0))
 
-canistra = s.Sprite("", (50,50), (-35, 580))
+canistra = s.Sprite("", (50, 50), (-35, 580))
 canistra.set_alpha(50)
 
 
-btn_interactive = s.Button("", (200,200), (950,750), "Взаимодействовать!", base_color= (255,100,100), on_click=on_click)
+btn_interactive = s.Button(
+    "",
+    (200, 200),
+    (950, 750),
+    "Взаимодействовать!",
+    base_color=(255, 100, 100),
+    on_click=on_click,
+)
 circle = s.utils.round_corners(btn_interactive.image, 500)
 btn_interactive.set_image(circle)
 btn_interactive.text_sprite.set_screen_space(True)
 btn_interactive.set_screen_space(True)
 
-interacts = [t1,t2,canistra]
+interacts = [t1, t2, canistra]
 
 img_canister = "spritePro\demoGames\Sprites\\canister.png"
 canistra_bg = s.Button(img_canister, (400, 400), s.WH_C, "", on_click=on_canistra)
@@ -69,7 +82,7 @@ canistra_max = 5
 canistra_bg.set_screen_space(True)
 canistra_bar.set_screen_space(True)
 canistra_bar.set_sorting_order(1010)
-canistra_bar.set_color((255,255,100))
+canistra_bar.set_color((255, 255, 100))
 canistra_bar.set_alpha(100)
 canistra_bar.set_position(canistra_bg.rect.bottomleft, s.Anchor.BOTTOM_LEFT)
 canistra_bar.rect.x += 100
@@ -80,10 +93,24 @@ canistra_bar.set_active(False)
 
 k_space = False
 
+
+
+walls = [s.Sprite("", (400, 50), (430, 370)),
+         s.Sprite("", (230, 70), (330, 550)),
+         s.Sprite("", (80, 70), (600, 550)),
+         s.Sprite("", (80, 70), (700, 350))]
+for w in walls:
+    w.set_alpha(100)
+    w.set_color((255,0,255))
+
+text_debug = s.TextSprite("debug", 24)
+text_debug.set_position((s.WH_C.x, s.WH.y - 50), s.Anchor.MID_BOTTOM)
+text_debug.set_screen_space(True)
+
 # Main game loop
 while True:
     k_space = False
-    
+
     # for e in s.events:
     #     if e.type == pygame.KEYDOWN:
     #         if e.key == pygame.K_SPACE:
@@ -92,28 +119,45 @@ while True:
     #         else:
     #             k_space = False
 
-
-    s.update(fill_color=(0,0,0))
+    s.update(fill_color=(0, 0, 0))
     player.handle_keyboard_input()
     s.set_camera_follow(player)
     text_cord.text = f"player cord: x:{player.rect.centerx}, y:{player.rect.centery}"
 
     if k_space:
-        if t1.rect.colliderect(player.rect):
+        if t1.rect.colliderect(player.collide.rect):
             player.set_position(t2.rect.center)
-        elif t2.rect.colliderect(player.rect):
+        elif t2.rect.colliderect(player.collide.rect):
             player.set_position(t1.rect.center)
-        elif canistra.rect.colliderect(player.rect):
+        elif canistra.rect.colliderect(player.collide.rect):
             cacanistra_count = 0
-            canistra_bar.set_image("", (200, cacanistra_count/canistra_max*250))
+            canistra_bar.set_image("", (200, cacanistra_count / canistra_max * 250))
             canistra_bg.set_active(True)
             canistra_bar.set_active(True)
         k_space = False
 
     colide = False
     for i in interacts:
-        if i.rect.colliderect(player.rect):
+        if i.rect.colliderect(player.collide.rect):
             colide = True
             break
 
     btn_interactive.set_active(colide)
+    collide = False
+    for w in walls:
+        if player.collide.rect.colliderect(w.rect):
+            collide = True
+            if player.rect.y > w.rect.y:
+                if player.velocity.y < 0:
+                    player.velocity.y = 0
+            if player.rect.y < w.rect.y:
+                if player.velocity.y > 0:
+                    player.velocity.y = 0
+            if player.rect.x > w.rect.x:
+                if player.velocity.x < 0:
+                    player.velocity.x = 0
+            if player.rect.x < w.rect.x:
+                if player.velocity.x > 0:
+                    player.velocity.x = 0
+
+    text_debug.text = f"collide: {collide}, velosity: {player.velocity}"
--- spritePro/demoGames/easy_clicker.py
+++ spritePro/demoGames/easy_clicker.py
@@ -58,8 +58,8 @@ class Btn_skin(s.Button):
 
 def onclick():
     global score
-    score += 1
-    money.add(1)
+    score += click
+    money.add(click)
     s.PlayerPrefs.set_int("score", score)
     emitter.set_position(s.WH_C)
     emitter.emit()
@@ -67,6 +67,8 @@ def onclick():
 
 def use_skin(sprite):
     player.set_image(sprite)
+    player.set_image(s.utils.round_corners(player.image, 5000))
+
 
 
 def go_game():
@@ -79,19 +81,27 @@ def go_shop():
     game_state = SHOP
 
 
+def go_upgrade():
+    global game_state
+    game_state = UPGRADE
+
+
 skin_id = s.PlayerPrefs.get_int("skin_id", 0)
 score = s.PlayerPrefs.get_int("score", 0)
 money = Wallet(score)
+click = 1
 
 SPACE = 50
 
 GAME = 0
 SHOP = 1
+UPGRADE = 2
 
 game_state = GAME
 
 sprite_skins = [
-    "spritePro\\demoGames\\Sprites\\c.png",
+    "spritePro\\demoGames\\Sprites\\bg1.jpg",
+    #"spritePro\\demoGames\\Sprites\\c.png",
     "spritePro\\demoGames\\Sprites\\door.png",
     "spritePro\\demoGames\\Sprites\\platforma.png",
     "spritePro\\demoGames\\Sprites\\fog.png",
@@ -107,25 +117,41 @@ s.init()
 s.get_screen((1280, 960), "My Game")
 
 bg = s.Sprite("spritePro\\demoGames\\Sprites\\bg1.jpg", s.WH, s.WH_C)
+bg.set_sorting_order(-1000)
 # Create a basic sprite
 emitter = s.ParticleEmitter(
     s.ParticleConfig(
         amount=100,
-        size_range=(5, 15),
-        speed_range=(800, 800),
+        image_scale_range=(0.5, 0.5),
+        speed_range=(1200, 2000),
         colors=[(255, 0, 0), (0, 0, 0), (255, 0, 255)],
         gravity=pygame.math.Vector2(0, 0),
+        fade_speed=500,
+        angular_velocity_range=(-1000, 1000),
+        spawn_circle_radius=100
     )
 )
+
 player = s.Button(sprite_skins[skin_id], (500, 500), s.WH_C, "", on_click=onclick)
+
 text = s.TextSprite(f"{score}", 96, (255, 0, 0))
 text.set_position((s.WH_C.x, SPACE), s.Anchor.MID_TOP)
 
-btn_shop = s.Button("", text="Shop", text_size=56, on_click=go_shop)
-btn_shop.set_position((SPACE, s.WH.y - SPACE), s.Anchor.BOTTOM_LEFT)
+btn_go_shop = s.Button("", text="Shop", text_size=56, on_click=go_shop)
+btn_go_shop.set_position((SPACE, s.WH.y - SPACE), s.Anchor.BOTTOM_LEFT)
+btn_go_shop.set_image(s.utils.round_corners(btn_go_shop.image, 50))
 
-btn_back = s.Button("", text="Back", text_size=56, on_click=go_game)
-btn_back.set_position((SPACE, s.WH.y - SPACE), s.Anchor.BOTTOM_LEFT)
+btn_go_back = s.Button("", text="Back", text_size=56, on_click=go_game)
+btn_go_back.set_position((SPACE, s.WH.y - SPACE), s.Anchor.BOTTOM_LEFT)
+btn_go_back.set_image(s.utils.round_corners(btn_go_back.image, 50))
+
+btn_go_upgrade = s.Button("", text="Upgrade", text_size=56, on_click=go_upgrade)
+btn_go_upgrade.set_position((s.WH_C.x, s.WH.y - SPACE), s.Anchor.MID_BOTTOM)
+#btn_upgrade.set_position((s.WH.x - SPACE, 0 +SPACE), s.Anchor.TOP_RIGHT)
+btn_go_upgrade.set_image(s.utils.round_corners(btn_go_upgrade.image, 50))
+
+btn_upgrade = s.Button("", (550, 150), s.WH_C, text="buy: 20, upgrade 1 > 2", text_size=56, on_click=go_upgrade)
+btn_upgrade.set_image(s.utils.round_corners(btn_upgrade.image, 50))
 
 x_start, y_start = 500, 300
 size = 200, 200
@@ -139,8 +165,9 @@ for i, e in enumerate(sprite_skins):
     skin.set_position((x_start + grid_space * x, y_start + grid_space * y))
     btn_skins.append(skin)
 
-sprites_game = [player, btn_shop]
-sprites_shop = [] + btn_skins
+sprites_game = [player, btn_go_shop]
+sprites_shop = [btn_go_upgrade] + btn_skins
+sprites_upgrade = [btn_upgrade]
 
 
 # Main game loop
@@ -148,10 +175,11 @@ while True:
     s.update()
 
     text.text = str(score)
+    emitter.config.image = player.image
+    emitter.config.amount = click
 
-    bg.set_color((255, 255, 255) if game_state == GAME else (100, 100, 100))
-    btn_back.set_active(game_state != GAME)
-    text.set_color(s.utils.ColorEffects.strobe(15, (255, 0, 0), (150, 0, 0)))
+    btn_go_back.set_active(game_state != GAME)
+    text.set_color(s.utils.ColorEffects.strobe(15, (255, 0, 0), (150, 0, 0),0.9))
 
     for sprite in sprites_game:
         sprite.set_active(game_state == GAME)
@@ -159,7 +187,12 @@ while True:
     for sprite in sprites_shop:
         sprite.set_active(game_state == SHOP)
 
+    for spite in sprites_upgrade:
+        spite.set_active(game_state == UPGRADE)
+
     if game_state == GAME:
-        pass
+        bg.set_color((100,100,100))
     elif game_state == SHOP:
-        pass
+        bg.set_color((255, 255, 255))
+    elif game_state == UPGRADE:
+        bg.set_color((100, 100, 255))
